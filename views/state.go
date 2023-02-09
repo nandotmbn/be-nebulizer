caf96d8b063fa34bfa02f756be4c4a9ffb01bab5
nandotmbn <orlandopratama695@gmail.com>
2023-02-10 01:52:54 +0700

    fix: state datatype

--- views/state.go
+++ views/state.go
@@ -3,19 +3,19 @@ package views
 import "time"
 
 type PayloadState struct {
-	State   int     `json:"state" validate:"min=0,max=5"`
-	Battery float32 `json:"battery" validate:"required"`
+	State   int     `json:"state"`
+	Battery float32 `json:"battery"`
 }
 
 type LastState struct {
-	State     int       `json:"state" validate:"required,min=0,max=5"`
+	State     int       `json:"state"`
 	Battery   float32   `json:"battery"`
 	UpdatedAt time.Time `json:"updated_at" bson:"updated_at"`
 	CreatedAt time.Time `json:"created_at" bson:"created_at"`
 }
 
 type FinalState struct {
-	State     int       `json:"state" validate:"required,min=0,max=5"`
+	State     int       `json:"state"`
 	Battery   float32   `json:"battery"`
 	UpdatedAt time.Time `json:"updated_at" bson:"created_at"`
 }
